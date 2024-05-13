4915e8342755e0577760bbca8eba0437346a388d
ibame7 <antoniodavidibanezmedina@gmail.com>
2024-05-13 12:51:18 +0200

    sda

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ session_start();
     <title>Reserva Y Juega</title>
     <link rel="icon" type="image/x-icon" href="imagenes/icono.ico" />
     <link rel="stylesheet" href="estilos/stylePrincipal.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body id="page-top">
@@ -195,6 +196,8 @@ session_start();
 </body>
 <script src="js/scriptPrincipal.js"></script>
 <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+
 
 </html>
 </body>
